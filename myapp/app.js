4f5be8b1ee642ce35c657b18e5abb93eca0059ad
Dario Arias <h.dario95@gmail.com>
2016-11-30 14:09:50 -0500

    Enviar Jason

--- myapp/app.js
+++ myapp/app.js
@@ -2,7 +2,29 @@ var express = require('express');
 var app = express();
 
 app.get('/Javascript', function (req, res) {
-  res.send('Hello World!');
+//request=req
+ 
+  console.log('Lo que tengo en el req!');
+console.log(req);
+  console.log('Lo que tengo en el res!');
+    console.log(res);
+    
+      console.log('Cabeceras req!');
+console.log(req.headers);
+  console.log('Cabeceras res!');
+    console.log(res.headers);
+  //response=res
+    
+    var usuario ={
+        nombre: 'Homero',
+        cedula:'1751230820'
+        
+    }
+    usuario.apellido='Arias';
+    usuario.mascotas=["perro","gato"];
+    usuario.casado='no';
+    res.json(usuario);
+ // res.send('Hello World!');
 });
 app.post('/Javascript', function (req, res) {
   res.send('Metodo POST!');
