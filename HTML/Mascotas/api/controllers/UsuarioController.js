8e7bec88fc4f58b03e6a1e6359c15f043f58fd27
Dario Arias <h.dario95@gmail.com>
2017-01-10 14:38:02 -0500

    AdministradorController

--- HTML/Mascotas/api/controllers/UsuarioController.js
+++ HTML/Mascotas/api/controllers/UsuarioController.js
@@ -7,6 +7,34 @@
 
 module.exports = {
 	
+//acceder al siguiente metodo /Usuario/crearUsuario
 
+
+    crearUsuario: function (req, res) {
+        //guardar todos los parametros en una variable parametros
+        var parametros = req.allParams();
+        console.log(parametros);
+        //
+        if(req.method=='POST'){
+        if(parametros.nombres&&parametros.apellidos){
+          Usuario.create({
+            nombres: parametros.nombres,
+            apellidos: parametros.apellidos,
+            correo: parametros.correo
+        }).exec(function (err, usuarioCreado) {
+            if (err) {
+                return res.serverError(err);
+            }
+            sails.log.info(usuarioCreado);
+            return res.ok(usuarioCreado); //200
+        });  
+        }else{
+            return res.badRequest('No envia todos los parametros');
+        }
+        }else{
+            
+            return res.badRequest('Metodo invalido')
+        }
+    }
 };
 
