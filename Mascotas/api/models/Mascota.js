ce0037ff5e84992f1741e825585a32858c56f1a2
Dario Arias <h.dario95@gmail.com>
2017-03-01 23:02:06 -0500

    CRUD

--- Mascotas/api/models/Mascota.js
+++ Mascotas/api/models/Mascota.js
@@ -8,7 +8,6 @@
 module.exports = {
 
   attributes: {
-      
       nombre:{
           type:'string',
           required:true
@@ -20,14 +19,10 @@ module.exports = {
           type:'string',
           enum:['Ecuador','Peru','Colombia']
       },
-      // idRaza es el nombre del Foreign Key
       idRaza:{
-          // Model-> es el nombre de la tabla padre
           model: 'Raza',
-          // Required es OPCIONAL si no queremos registros huerfanos de raza
           required:true
       }
-
   }
 };
 
